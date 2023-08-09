206463a98bcaded452b4fdf1632496061204d624
robin gupta <smilin.robin@gmail.com>
2023-08-09 23:56:34 +0530

    Updates

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -2,6 +2,7 @@ import { LightningElement } from 'lwc';
 export default class HelloWorld extends LightningElement {
   greeting = 'World';
   changeHandler(event) {
+    // get the greeting value
     this.greeting = event.target.value;
   }
 }
